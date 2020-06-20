ee481b9d2e4314075add007fdf5b1343307fa0d4
HDubuis <helene.dubuis@heig-vd.ch>
2020-06-21 00:45:44 +0200

    Pour plus de clareté : transformation du for de statement en foreach, et donc supression des itérateur.

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -13,23 +13,20 @@ string Customer::statement()
 {
     double totalAmount = 0;
     int frequentRenterPoints = 0;
-    vector< Rental >::iterator iter = _rentals.begin();
-    vector< Rental >::iterator iter_end = _rentals.end();
     ostringstream result;
     result << "Rental Record for " << getName() << "\n";
-    for ( ; iter != iter_end; ++iter ) {
+    for ( const Rental &rental : _rentals ) {
         double thisAmount = 0;
-        Rental each = *iter;
-        thisAmount += each.getAmount();
+        thisAmount += rental.getAmount();
 
         // add frequent renter points
         frequentRenterPoints++;
         // add bonus for a two day new release rental
-        if ( ( each.getMovie().getPriceCode() == Movie::NEW_RELEASE )
-             && each.getDaysRented() > 1 ) frequentRenterPoints++;
+        if ( ( rental.getMovie().getPriceCode() == Movie::NEW_RELEASE )
+             && rental.getDaysRented() > 1 ) frequentRenterPoints++;
 
         // show figures for this rental
-        result << "\t" << each.getMovie().getTitle() << "\t"
+        result << "\t" << rental.getMovie().getTitle() << "\t"
                << thisAmount << "\n";
         totalAmount += thisAmount;
     }
