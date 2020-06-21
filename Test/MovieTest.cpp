06439900290410679c8947e3ef3db478e4e08779
HDubuis <helene.dubuis@heig-vd.ch>
2020-06-21 11:27:45 +0200

    Add a test for Movie

--- Test/MovieTest.cpp
+++ Test/MovieTest.cpp
@@ -1,2 +1,15 @@
 #include "gtest/gtest.h"
-#include "gmock/gmock.h" 
+#include "gmock/gmock.h"
+
+
+#include "../src/Movie.h"
+
+TEST(movie_test, TestMovieName){
+
+    Movie movie("Karate Kid");
+
+    std::string resultat = movie.getTitle();
+    std::string attendu = "Karate Kid";
+
+    EXPECT_EQ(resultat, attendu);
+}
